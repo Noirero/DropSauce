0313741e2ca759fbd06e5e794fd74c420e806e24
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-01 10:23:20 +0700

    Store extended manga metadata overrides

--- app/src/main/kotlin/org/koitharu/kotatsu/core/db/entity/MangaPrefsEntity.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/db/entity/MangaPrefsEntity.kt
@@ -30,5 +30,8 @@ data class MangaPrefsEntity(
 	@ColumnInfo(name = "title_override") val titleOverride: String?,
 	@ColumnInfo(name = "cover_override") val coverUrlOverride: String?,
 	@ColumnInfo(name = "content_rating_override") val contentRatingOverride: String?,
+	@ColumnInfo(name = "author_override") val authorOverride: String?,
+	@ColumnInfo(name = "artist_override") val artistOverride: String?,
+	@ColumnInfo(name = "description_override") val descriptionOverride: String?,
 	@ColumnInfo(name = "merge_scanlators", defaultValue = "0") val mergeScanlators: Boolean = false,
 )
