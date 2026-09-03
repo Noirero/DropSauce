5eeb5161fcf55b9fb9ef094d5ec186f64208b414
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-03 08:52:08 +0700

    Simplify Explore overflow after filter move

--- app/src/main/kotlin/org/koitharu/kotatsu/explore/ui/ExploreMenuProvider.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/explore/ui/ExploreMenuProvider.kt
@@ -6,7 +6,6 @@ import android.view.MenuItem
 import androidx.core.view.MenuProvider
 import org.koitharu.kotatsu.R
 import org.koitharu.kotatsu.core.nav.AppRouter
-import org.koitharu.kotatsu.explore.data.ExploreContentFilter
 
 class ExploreMenuProvider(
 	private val router: AppRouter,
@@ -18,34 +17,8 @@ class ExploreMenuProvider(
 		menuInflater.inflate(R.menu.opt_explore, menu)
 	}
 
-	override fun onPrepareMenu(menu: Menu) {
-		when (viewModel.contentFilter.value) {
-			ExploreContentFilter.ALL -> menu.findItem(R.id.action_content_filter_all)?.isChecked = true
-			ExploreContentFilter.SFW -> menu.findItem(R.id.action_content_filter_sfw)?.isChecked = true
-			ExploreContentFilter.NSFW -> menu.findItem(R.id.action_content_filter_nsfw)?.isChecked = true
-		}
-	}
-
 	override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
 		return when (menuItem.itemId) {
-			R.id.action_content_filter_all -> {
-				viewModel.setContentFilter(ExploreContentFilter.ALL)
-				menuItem.isChecked = true
-				true
-			}
-
-			R.id.action_content_filter_sfw -> {
-				viewModel.setContentFilter(ExploreContentFilter.SFW)
-				menuItem.isChecked = true
-				true
-			}
-
-			R.id.action_content_filter_nsfw -> {
-				viewModel.setContentFilter(ExploreContentFilter.NSFW)
-				menuItem.isChecked = true
-				true
-			}
-
 			R.id.action_content_classification_reset_all -> {
 				viewModel.resetContentClassifications()
 				true
